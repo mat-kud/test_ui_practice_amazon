4cbea4374bd0f0c785043f35c2c100273240add2
mm <henrykus888@gmail.com>
2022-12-03 15:27:01 +0100

    -added dependency to solve issue with new browser version

--- src/main/java/org/example/pageobjects/MainPage.java
+++ src/main/java/org/example/pageobjects/MainPage.java
@@ -4,6 +4,7 @@ import org.example.pageobjects.modules.DeliverToLocationPopUpModule;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 
 public class MainPage extends BasePage {
